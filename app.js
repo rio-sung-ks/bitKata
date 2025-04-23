91f2408f8c61d790a51948f9c07a81d70a5e9ecb
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2025-04-23 23:45:20 +0900

    Problem List - moving problem/:id

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function connectDB() {
 }
 connectDB();
 const problems = await Problem.find();
-// console.log(problems);
+console.log(problems);
 // mongodb+srv://riosungks:1Qawsedrf@mongodb-cluster.dqjg6hw.mongodb.net/rio-api?retryWrites=true&w=majority&appName=mongodb-cluster
 import express from "express";
 import session from "express-session";
@@ -35,21 +35,6 @@ app.set("view engine", "ejs",);
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
-
-
-
-// initialize => serialize => authenticate
-//사용자가 로그인 양식을 제출하면 /login으로 POST 요청이 전송되어 설정한 passport.authenticate 미들웨어가 실행됩니다.
-// Passport는 req.body.username 및 req.body.password를 가져와 로컬 전략의 확인 함수에 전달합니다.
-// 데이터베이스에서 사용자를 로드하고 주어진 비밀번호가 데이터베이스에 있는 비밀번호와 일치하는지 확인하는 작업을 수행합니다.
-
-//사용자를 찾을 수 없거나 비밀번호가 확인되지 않으면 done(null, false)를 호출합니다.
-// 모든 것이 정상적으로 진행되어 사용자가 로그인하기를 원하면 done(null, user)를 호출합니다.
-// done를 호출하면 흐름이 passport.authenticate로 다시 이동합니다. 여기에는 오류, 사용자 및 추가 정보 객체(정의된 경우)가 전달
-//사용자가 전달되면 미들웨어는 req.login(요청에 첨부된 패스포트 함수)을 호출합니다.
-
-// TODO: 필요에 따라 세션 및 Passport 관련 설정을 수정해주세요.
-
 app.use(
   session({
   secret: "sampleSecretKey",
@@ -78,9 +63,9 @@ app.get(
 app.use("/", index);
 app.use("/problems/:id", (req, res, next) => {
   const problemId = req.params.id;
-  console.log(problemId);
+  // console.log(problemId);
   // console.log(sample[0]);
-  res.json(sample_problems[0]);
+  res.json(problems[problemId]);
 
 });
 
