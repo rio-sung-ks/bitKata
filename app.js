ca87bf3b0f0b36ba733332ccdb4bb0cf6ef59d93
rio.sung.ks@gmail.com <rio.sung.ks@gmail.com>
2025-04-25 02:16:33 +0900

    Evaluating Code - failure.ejs

--- app.js
+++ app.js
@@ -44,17 +44,13 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-//세션에 사용자 정보가 있으면 deserializeUser 호출
 
 configPassport();
 app.get("/auth/github", passport.authenticate("github"));
-
-
 app.get(
   "/auth/github/callback",
   passport.authenticate("github", { failureRedirect: "/" }),
   function (req, res, next) {
-    // Successful authentication, redirect home.
     res.redirect("/");
   }
 );
@@ -80,48 +76,50 @@ app.post("/problems/:id", (req, res) => {
   const proId = req.body.problemId;
   const codeText = req.body.code;
   let noTest = problems[proId].tests;
-  console.log(`noTest : ${noTest}`);
-  // const arg = problems[proId].tests[proId].code;
-  // const answer =  problems[proId].tests[proId].solution;
-  
-  console.log("proId : ", proId); // 0
-  console.log("codeText : ", codeText); // function
-  // console.log("function call : ", arg);  // solution(3)
-  // console.log("return value : ", answer);  // 2
-  
+  const failArray = [];
+
+  for (let i = 0; i < noTest.length; i++) {
   try {
-    for (let i = 0; i < noTest.length; i++) {
       let arg = problems[proId].tests[i].code;
       let answer = problems[proId].tests[i].solution;
-      // console.log("function call : ", arg);  // solution(3)
-      // console.log("return value : ", answer);  // 2
       const context = {};
       vm.createContext(context);
       vm.runInContext(codeText,context);
       const result = vm.runInContext(arg,context);
 
-      if(result === answer){
-        return res.send("success");
-      } else {
-        return res.send("failure");
+      if(result !== answer){
+        failArray.push(i);
       }
+      console.log(`failArray : ${failArray}`);
+    } catch (error) {
+      failArray.push(i);
     }
-    
-    // console.log(`result : ${result}`);
-    // console.log(`result : ${answer}`)
-
-  } catch (error) {
-    res.send("failure");
-
+  }
+  if(failArray.length === 0){
+    res.send("success");
+  } else {
+    res.render("failure", { failArray: failArray, proId: proId, problems: problems });
   }
 })
 
+// 피보나치
 // function solution(n) {
 //   let fib = [0, 1];
 //   for (let i = 2; i <= n; i++) {
 //       fib[i] = (fib[i-1] + fib[i-2]) % 1234567;
 //   }
 //   return fib[n];
+// }
+
+// 김서방
+// function solution(seoul){
+//   var idx = seoul.indexOf('Kim');
+//   return "김서방은 " + idx + "에 있다";
+// }
+
+// 수박수박수박수박수
+// function solution(n) {
+//     return "수박".repeat(n).slice(0, n);
 // }
 
 //하샤드
